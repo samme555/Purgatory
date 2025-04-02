a31e40185af96cbceefcfdf754337f71a0d56138
Pal <palika.szasz@gmail.com>
2025-04-02 18:08:21 +0200

    Destruction
    Vases are kinda destructible
    
    Co-Authored-By: adamzpko <180215974+adamzpko@users.noreply.github.com>

--- Purgatory/Assets/DecoDestruction.cs
+++ Purgatory/Assets/DecoDestruction.cs
@@ -1,24 +1,24 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class DecoDestruction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Tilemap vaseTilemap;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("WalkDestructible"))
+        if (other.gameObject == vaseTilemap.gameObject)
         {
-            Destroy(collision.gameObject);
+            Vector3 contactPoint = other.ClosestPoint(transform.position);
+            Vector3Int cellPos = vaseTilemap.WorldToCell(contactPoint);
+
+            if (vaseTilemap.HasTile(cellPos))
+            {
+                Debug.Log("Destroying vase at: " + cellPos);
+                vaseTilemap.SetTile(cellPos, null);
+            }
         }
     }
 }
+
+
