b9eadb0a18038aaaaf2c2e05901e13a9bc373ed8
adamzpko <adam.oe.johnsson@outlook.com>
2025-04-02 14:09:58 +0200

    Fixad collision(?)

--- Purgatory/Assets/Scrips/Movement.cs
+++ Purgatory/Assets/Scrips/Movement.cs
@@ -1,22 +1,17 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] Rigidbody2D rb;
-    [SerializeField] LayerMask collisionLayer;
     private Vector2 movementDirection;
 
-    [Header("Raycast Settings")]
-    [SerializeField] float raycastDistance = 2f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
@@ -27,23 +22,6 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-
-        if (!IsBlocked(movementDirection))
-        {
-            rb.linearVelocity = movementDirection * moveSpeed;
-        }
-        else
-        {
-            rb.linearVelocity = Vector2.zero;
-        }
-
+        rb.linearVelocity = new Vector2(movementDirection.x * moveSpeed, movementDirection.y * moveSpeed);
     }
-
-    private bool IsBlocked(Vector2 direction)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(rb.position, direction, raycastDistance, collisionLayer);
-        return hit.collider != null; 
-    }
-
-
 }
