8d5f58871e5016fcd6ab2dcee3c5be9d18c9c621
OliverJ0709 <oliver.johansson8@gmail.com>
2025-04-02 16:32:44 +0200

    projectile
    +script for att skjuta projektiler

---
+++ Purgatory/Assets/Scrips/Player/Projectile/Shooting.cs
@@ -0,0 +1,54 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform firePoint;
+    public GameObject projectilePrefab;
+    public float attackSpeed = 1f;
+    public float bulletForce = 20f;
+    private float nextFireTime = 0f;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextFireTime) 
+        {
+            Shoot();
+            nextFireTime = Time.time + (1f / attackSpeed);
+        }
+    }
+
+    void Shoot() 
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
+
+        Vector2 direction = (mousePosition - firePoint.position).normalized;
+        
+
+
+        GameObject bullet = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = direction * bulletForce;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bullet.transform.rotation = Quaternion.Euler(0,0,angle);
+
+        Collider2D playerCollider = GetComponent<Collider2D>();
+        Collider2D bulletCollider = projectilePrefab.GetComponent<Collider2D>();
+
+        if (playerCollider != null && bulletCollider != null) 
+        {
+            Physics2D.IgnoreCollision(bulletCollider, playerCollider);
+        }
+
+        
+
+    }
+
+    
+}
