8e077fd2058b049a0806158f5d98f444506354c1
Pal <142800170+Morcos16@users.noreply.github.com>
2025-04-03 14:24:41 +0200

    Main Menu och TP mellan rum
    snabb mainmenu ihopsatt av Dam, tp mellan de rum vi har och ändrade layer på enemy
    
    Co-Authored-By: adamzpko <180215974+adamzpko@users.noreply.github.com>

---
+++ Purgatory/Assets/LevelMoveScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMoveScript : MonoBehaviour
+{
+    public int sceneBuildingIndex;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneBuildingIndex, LoadSceneMode.Single);
+        }
+    }
+}
