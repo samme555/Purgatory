81cbe316fea8a21c8336fa66e2221c478eb14c48
ki3ii <fikrusen@gmail.com>
2025-04-21 15:42:49 +0200

    ParticleSystem
    Fixed impact effects (changed projectile, added particlesystem for impact effekt, added duplicates of grid for trigger)

--- Purgatory/Assets/Scripts/Collisions.cs
+++ Purgatory/Assets/Scripts/Collisions.cs
@@ -2,17 +2,28 @@ using UnityEngine;
 
 public class Collisions : MonoBehaviour
 {
-    public int damage = 1;
+    [SerializeField] private int damage = 1;
+    [SerializeField] private GameObject impactEffect;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy"))
+        bool isEnemy = other.CompareTag("Enemy");
+        bool isWall = other.CompareTag("Wall");
+
+        if (isEnemy)
         {
             EnemyStats stats = other.GetComponent<EnemyStats>();
-
             if (stats != null)
             {
                 stats.TakeDamage(damage);
+            }
+        }
+
+        if (isEnemy || isWall)
+        {
+            if (impactEffect != null)
+            {
+                Instantiate(impactEffect, transform.position, Quaternion.identity);
             }
 
             Destroy(gameObject);
