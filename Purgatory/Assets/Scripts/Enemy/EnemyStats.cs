ab43fa82a71836c9b0369022941629feea9dbee0
adamzpko <adam.oe.johnsson@outlook.com>
2025-04-21 17:14:45 +0200

    Created healthbar
    added a healthbar to the enemy prefab and also adjusted the enemy health

--- Purgatory/Assets/Scripts/Enemy/EnemyStats.cs
+++ Purgatory/Assets/Scripts/Enemy/EnemyStats.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyStats : MonoBehaviour
 {
-    public int health = 1;
+    public float health;
+    float maxHealth;
+    public Image healthBar;
 
+    public void Start()
+    {
+        maxHealth = health;
+        UpdateHealthBar();
+    }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
+
+        UpdateHealthBar();
 
         if (health <= 0)
         {
             Die();
         }
     }
+
+    public void UpdateHealthBar()
+    {
+        healthBar.fillAmount = health / maxHealth;
+    }
+
     private void Die()
     {
         Destroy(gameObject);
