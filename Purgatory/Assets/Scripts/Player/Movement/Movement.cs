ce27e7d39b9fde3b0f85f882bbda0ec473d31525
adamzpko <adam.oe.johnsson@outlook.com>
2025-04-25 15:06:38 +0200

    Wall collisions
    Removed raycasting for player/wall collisions.

--- Purgatory/Assets/Scripts/Player/Movement/Movement.cs
+++ Purgatory/Assets/Scripts/Player/Movement/Movement.cs
@@ -7,10 +7,10 @@ public class Movement : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] Rigidbody2D rb;
 
-    public ContactFilter2D movementFilter; //we use movement filter to detect walls, defined in script inspector.
-    public float collisionOffset = 0.05f; //small offset to prevent wall-clipping
+    //public ContactFilter2D movementFilter; //we use movement filter to detect walls, defined in script inspector.
+    //public float collisionOffset = 0.05f; //small offset to prevent wall-clipping
 
-    private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>(); //store raycasts
+    //private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>(); //store raycasts
 
     public Animator anim;
 
@@ -38,49 +38,9 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (input != Vector2.zero) //only move is input detected
+        if (input != Vector2.zero)
         {
-            float moveDistance = moveSpeed * Time.fixedDeltaTime + collisionOffset; //calculate how far we move in one frame
-
-            castCollisions.Clear(); //clear list every iteration to get proper hit detection
-            int count = rb.Cast(input, movementFilter, castCollisions, moveDistance); //try to move in full direction based on input
-
-            if (count == 0) //path clear, CAN move full distance
-            {
-                rb.MovePosition(rb.position + input * moveSpeed * Time.fixedDeltaTime);
-            }
-            else //full movement blocked, we can "slide" along wall. (prevents player from completely freezing when hitting a wall)
-            {
-                if (Mathf.Abs(input.x) > 0.01f) //try to move horizontally(x-axis) (based on input). 
-                    //if player stops, player doesn't slide. only attempts slide if input is detected.
-                {
-                    Vector2 moveX = new Vector2(input.x, 0).normalized;
-                    castCollisions.Clear();
-                    count = rb.Cast(moveX, movementFilter, castCollisions, moveDistance);
-
-                    if (count == 0) //horizontal path clear, can slide along x-axis
-                    {
-                        rb.MovePosition(rb.position + moveX * moveSpeed * Time.fixedDeltaTime);
-                        return;
-                    }
-                }
-
-                
-                if (Mathf.Abs(input.y) > 0.01f) //slide along vertical (y-axis)
-                {
-                    Vector2 moveY = new Vector2(0, input.y).normalized;
-                    castCollisions.Clear();
-                    count = rb.Cast(moveY, movementFilter, castCollisions, moveDistance);
-
-                    if (count == 0)
-                    {
-                        rb.MovePosition(rb.position + moveY * moveSpeed * Time.fixedDeltaTime);
-                        return;
-                    }
-                }
-
-                Debug.Log("Player completely blocked"); //nowhere to go (shouldn't happen)
-            }
+            rb.MovePosition(rb.position + input * moveSpeed * Time.fixedDeltaTime);
         }
     }
 
