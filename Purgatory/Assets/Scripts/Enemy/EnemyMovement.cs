c997e2f5fcd0e1d0f47ae9085df075d268aea095
ki3ii <fikrusen@gmail.com>
2025-05-24 14:17:31 +0200

    Attack animation + attack logic
    Fixat attack animation + attack logic till orc?

--- Purgatory/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Purgatory/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -13,6 +14,21 @@ public class EnemyMovement : MonoBehaviour
     public float speed = 0.5f;
     private Transform player;
     public Animator anim;
+    public bool isAttacking = false;
+    private bool playerDetected = false;
+    public float attackRange = 0.7f;
+    public float attackCooldown = 1f;
+    public float aggroRange = 6f;
+    private float lastAttackTime;
+    public GameObject hitZone;
+    public OrcHitZone hitZoneScript;
+
+    
+    [SerializeField] private Vector2 hitOffsetUp = new Vector2(0f, 0.02f);
+    [SerializeField] private Vector2 hitOffsetDown = new Vector2(0f, -0.02f);
+    [SerializeField] private Vector2 hitOffsetSide = new Vector2(0.02f, 0f);
+    [SerializeField] private float hitZoneRadius = 0.3f;
+
 
     private Vector2 direction;
 
@@ -31,16 +47,118 @@ public class EnemyMovement : MonoBehaviour
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        if (hitZone != null)
+        {
+            var col = hitZone.GetComponent<CircleCollider2D>();
+            if (col != null)
+                col.radius = hitZoneRadius;
+        }
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        if (player != null)
+        if (player == null || agent.pathPending) return;
+
+        float trueDistance = Vector2.Distance(transform.position, player.position);
+
+        if (!playerDetected && trueDistance <= aggroRange)
+            playerDetected = true;
+
+        if (!playerDetected) return;
+
+        float dist = Vector2.Distance(transform.position, player.position);
+
+        if (!isAttacking && dist <= attackRange && Time.time >= lastAttackTime + attackCooldown)
         {
-            agent.SetDestination(player.position);
-            Animate(agent.velocity.normalized);
+            lastAttackTime = Time.time;
+            StartCoroutine(PerformAttack(player.gameObject));
         }
+
+        if (!isAttacking)
+        {
+            if (dist > attackRange)
+            {
+                agent.isStopped = false;
+                agent.SetDestination(player.position);
+                Animate(agent.velocity.normalized);
+            }
+            else
+            {
+                agent.isStopped = true;
+                Animate(Vector2.zero);
+            }
+        }
+        else
+        {
+            agent.isStopped = true;
+            Animate(Vector2.zero);
+        }
+    }
+
+    public void EnableDamage()
+    {
+        hitZoneScript?.EnableDamage();
+    }
+
+    public void DisableDamage()
+    {
+        hitZoneScript?.DisableDamage();
+    }
+
+    public void TriggerAttack()
+    {
+        if(anim != null)
+        {
+            Vector2 dir = (player.position - transform.position).normalized;
+
+            anim.SetFloat("X", dir.x);
+            anim.SetFloat("Y", dir.y);
+            anim.SetTrigger("Attack");
+
+            if(dir.x < 0) spriteRenderer.flipX = true;
+            else if (dir.x > 0) spriteRenderer.flipX = false;
+
+            if (hitZone != null)
+            {
+                if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+                {
+                    float sideDir = dir.x < 0 ? -1f : 1f;
+                    hitZone.transform.localPosition = new Vector2(hitOffsetSide.x * sideDir, hitOffsetSide.y);
+                }
+                else
+                {
+                    hitZone.transform.localPosition = dir.y > 0 ? hitOffsetUp : hitOffsetDown;
+                }
+            }
+        }
+        Debug.Log("HitZone new pos: " + hitZone.transform.localPosition);
+
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        StartCoroutine(PerformAttack(collision.gameObject));
+    //    }
+    //}
+
+    private IEnumerator PerformAttack(GameObject target)
+    {
+        isAttacking = true;
+        agent.isStopped = true;        
+
+        TriggerAttack();
+
+        target.GetComponent<PlayerStats>()?.TakeDamage(1);
+
+        yield return new WaitForSeconds(0.6f);
+
+        
+        agent.isStopped = false;
+        isAttacking = false;
     }
 
     private void Animate(Vector2 dir)
